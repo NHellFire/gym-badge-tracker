8656462cdd93925e194d45078f32a9df0aea4d92
Nathan Rennie-Waldock <nathan.renniewaldock@gmail.com>
2019-10-28 02:26:09 +0000

    Remove scripts

--- scripts/pretty-print.php
+++
@@ -1,5 +0,0 @@
-<?php
-// This just pretty prints gyms.json so we don't get a huge diff
-
-$file = "gyms.json";
-file_put_contents($file, json_encode(json_decode(file_get_contents($file), true), JSON_PRETTY_PRINT));
